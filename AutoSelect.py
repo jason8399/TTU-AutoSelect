ebaa9f6a840920d9e06c11dedb81fbcd86e1e73b
JasonPan <jason8399@gmail.com>
2014-05-01 01:59:18 +0800

    set time done!! retry if the course can't be selected

--- AutoSelect.py
+++ AutoSelect.py
@@ -5,6 +5,7 @@ import http.cookiejar
 import http.client
 import platform
 import os
+import time
 
 ########################################副程式區######################################################
 def showMenu():
@@ -28,15 +29,16 @@ def webDecodeBig5(web_string):			#web資料Decode BIG5
 def Login():
 	x = True
 	while x:
-		#ID = input("Please input student ID: ")
-		#PWD = input("Please input Password: ")
-		ID = "410206108"
-		PWD = "buffald123"
+		ID = input("Please input student ID: ")
+		PWD = input("Please input Password: ")
 		data = {"ID": ID, "PWD": PWD, "Submit": "登入系統"}
 		data = urllib.parse.urlencode(data)
 		data = data.encode('big5')
 		req = urllib.request.Request(url_login, data, header, None, None, 'POST')
-		print(urllib.request.urlopen(req).status)
+		print(urllib.request.urlopen(req).getheaders())
+		######
+		print(time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()) + ' GMT')
+		######
 		if ('登入錯誤' in webDecodeBig5(urllib.request.urlopen(req))):
 			print('登入錯誤')
 			x = True
@@ -72,27 +74,55 @@ def selectCourse():
 	courseID = selectList.readlines()
 	for x in range(0, len(courseID)):
 		courseID[x] = courseID[x].strip('\n')
+	YN = input("Do you want use CountDown function?(y/n)\n")
+	if(YN == "Y" or YN == "y"):
+		settime()
 	for x in range(0, len(courseID)):
-		selectID_url = url_select % ("AddSbjNo=%s" % courseID[x])
+		selectID_url = url_select + "AddSbjNo=" + courseID[x]
 		print(selectID_url)
+		while(True):
+			try:
+				req = urllib.request.Request(selectID_url, None, header, None, None, 'GET')
+				u = urllib.request.urlopen(req)
+				web_string = webDecodeBig5(u)
+				print(u.status)
+				print(u.getheaders())
+				print(web_string)
+				print("Select suecced")
+				break
+			except urllib.error.HTTPError as err:
+				print(err.reason)
+				print("retry select " + courseID[x])
+	
+def settime():
+	flag = True
+	while(flag):
+		user_Time_Str = input("Please input YYYY/MM/DD HH:MM:SS\n")
+		user_Time_Str += " GMT +0800"
 		try:
-			req = urllib.request.Request(selectID_url, None, header, None, None, 'GET')
-			u = urllib.request.urlopen(req)
-			web_string = webDecodeBig5(u)
-			print(u.status)
-			print(u.getheaders())
-			print(web_string)
-		except urllib.error.HTTPError as err:
-			print(err.reason)
-	
-	
+			time_set = time.strptime(user_Time_Str, "%Y/%m/%d %H:%M:%S %Z %z")
+			flag = False
+		except Exception:
+			print("Set time ERROR\ntry again.")
+	time_set_str = time.strftime("%Y/%m/%d %H:%M:%S", time_set)
+	while(True):
+		time_Temp = time.localtime()
+		while(True):
+			if(time_Temp < time.localtime()):
+				break;
+		clrscr()
+		print("Now Time Is  " + time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()))
+		print("The Time Set " + time_set_str)
+		if(time.localtime() > time_set):
+			break;
+	print("time up!!!!!!")
 
 
 
 #########################################物件區######################################################
 url_login = 'http://stucis.ttu.edu.tw/login.php'
 url_listed = 'http://stucis.ttu.edu.tw/selcourse/ListSelected.php'
-url_select = 'http://stucis.ttu.edu.tw/selcourse/DoAddDelSbj.php?%s'
+url_select = 'http://stucis.ttu.edu.tw/selcourse/DoAddDelSbj.php?'
 header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1897.3 Safari/537.36"}
 cookie = http.cookiejar.CookieJar()
 flag = True
