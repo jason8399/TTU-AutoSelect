38f6a317c3cb7784ddb592796c85536cb2be3e2c
JasonPan <jason8399@gmail.com>
2014-07-30 19:13:15 +0800

    Add reflash and some syntax fix

--- AutoSelect.py
+++ AutoSelect.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import urllib.request
 import urllib.parse
 import urllib.response
@@ -6,20 +7,20 @@ import http.client
 import platform
 import os
 import time
-
+import getpass
 ########################################副程式區######################################################
 def showMenu():
-	print("Welcome To Use ASCS for TTU")
-	print("Please Selected The function")
-	print("1.Read Selected Course <dev>")
-	print("2.Select Course Process")
-	print("5.Exit")
+	print('Welcome To Use ASCS for TTU')
+	print('Please Selected The function')
+	print('1.Select Course Process')
+	print('2.Read Selected Course <dev>')
+	print('5.Exit')
 
 def clrscr():
 	system = platform.system()
-	if(system.lower() == 'windows'):
+	if system.lower() == 'windows':
 		os.system('cls')
-	elif(system.lower() == 'darwin'):
+	elif system.lower() == 'darwin':
 		os.system('clear')
 
 def webDecodeBig5(web_string):			#web資料Decode BIG5
@@ -29,31 +30,31 @@ def webDecodeBig5(web_string):			#web資料Decode BIG5
 def Login():
 	x = True
 	while x:
-		ID = input("Please input student ID: ")
-		PWD = input("Please input Password: ")
-		data = {"ID": ID, "PWD": PWD, "Submit": "登入系統"}
+		clrscr()
+		ID = input('Please input student ID: ')
+		PWD = getpass.getpass('Please input Password: ')
+		data = {'ID': ID, 'PWD': PWD, 'Submit': '登入系統'}
 		data = urllib.parse.urlencode(data)
 		data = data.encode('big5')
 		req = urllib.request.Request(url_login, data, header, None, None, 'POST')
 		#print(urllib.request.urlopen(req).getheaders())
 		######
-		#print(time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()) + ' GMT')
+		#print(time.strftime('%a, %d %b %Y %H:%M:%S', time.gmtime()) + ' GMT')
 		######
-		if ('登入錯誤' in webDecodeBig5(urllib.request.urlopen(req))):
-			print('登入錯誤')
+		if '登入錯誤' in webDecodeBig5(urllib.request.urlopen(req)):
+			print('failed')
 			x = True
-			clrscr()
 		else:
-			print('登入成功!!!!')
+			print('success!!!!')
 			x = False
-		input('任意鍵繼續......')
+		input('Please press any key to continue......')
 
 def readSelectedList():	#Select Course List
 	req = urllib.request.Request(url_listed, None, header, None, None, 'GET')
 	u = urllib.request.urlopen(req)
 	web_string = webDecodeBig5(urllib.request.urlopen(req))
-	if ('Not login or session expire!' in web_string):
-		print("You need to Login again........\n")
+	if 'Not login or session expire!' in web_string:
+		print('You need to Login again........\n')
 		Login()
 		readSelectedList()
 	else:
@@ -61,92 +62,94 @@ def readSelectedList():	#Select Course List
 
 def selectCourse():
 	flag = True
-	while(flag):
+	while flag:
 		clrscr()
-		print("Please Check \"list.txt\" is under same folder")
-		input('任意鍵繼續......')
+		print('Please Check "list.txt" is under same folder')
+		input('Please press any key to continue......')
 		try:
-			selectList = open("list.txt")
+			selectList = open('list.txt')
 			flag = False
 		except Exception:
-			print("Open \"list.txt\" fail.\nPlease Check Your file")
+			print('Open "list.txt" fail.\nPlease Check Your file')
+			input('Please press any key to continue......')
 			flag = True
 	courseID = selectList.readlines()
 	for x in range(0, len(courseID)):
 		courseID[x] = courseID[x].strip('\n')
-	YN = input("Do you want use CountDown function?(y/n)\n")
-	if(YN == "Y" or YN == "y"):
+	YN = input('Do you want use CountDown function?(y/n)\n')
+	if YN == 'Y' or YN == 'y':
 		settime()
 	for x in range(0, len(courseID)):
-		selectID_url = url_select + "AddSbjNo=" + courseID[x]
+		selectID_url = url_select + 'AddSbjNo=' + courseID[x]
 		#print(selectID_url)
-		while(True):
+		while True:
 			try:
 				req = urllib.request.Request(selectID_url, None, header, None, None, 'GET')
 				u = urllib.request.urlopen(req)
 				web_string = webDecodeBig5(u)
 				print(u.status)
-				print(u.getheaders())
+				#print(u.getheaders())
 				print(web_string)
-				print("Select suecced")
+				print('Select suecced')
 				break
 			except urllib.error.HTTPError as err:
 				print(err.reason)
-				print("retry select " + courseID[x])
+				print('retry select ' + courseID[x])
 	
 def settime():
-	flag = True
-	while(flag):
-		user_Time_Str = input("Please input YYYY/MM/DD HH:MM:SS\n")
-		user_Time_Str += " GMT +0800"
+	while True:
+		user_Time_Str = input('Please input YYYY/MM/DD HH:MM:SS\n')
+		user_Time_Str += ' GMT +0800'
 		try:
-			time_set = time.strptime(user_Time_Str, "%Y/%m/%d %H:%M:%S %Z %z")
-			flag = False
+			time_set = time.strptime(user_Time_Str, '%Y/%m/%d %H:%M:%S %Z %z')
+			if time_set < time.localtime():
+				raise Exception
+			break
 		except Exception:
-			print("Set time ERROR\ntry again.")
-	time_set_str = time.strftime("%Y/%m/%d %H:%M:%S", time_set)
-	while(True):
+			print('Set time ERROR\ntry again.\n')
+	time_set_str = time.strftime('%Y/%m/%d %H:%M:%S', time_set)
+	while True:
 		time_Temp = time.localtime()
-		while(True):
+		while True:
 			if(time_Temp < time.localtime()):
-				break;
+				break
 		clrscr()
-		print("Now Time Is  " + time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()))
-		print("The Time Set " + time_set_str)
-		if(time.localtime() > time_set):
+		print('Now Time Is  ' + time.strftime('%Y/%m/%d %H:%M:%S', time.localtime()))
+		print('The Time Set ' + time_set_str)
+		reflash()
+		if time.localtime() > time_set:
 			break;
-	print("time up!!!!!!")
+	print('time up!!!!!!')
 
+def reflash():
+	req = urllib.request.Request(url_listed, None, header, None, None, 'GET')
+	u = urllib.request.urlopen(req)
+	web_string = webDecodeBig5(u)
+	if 'Not login or session expire!' in web_string:
+		print('You need to Login again........\n')
+		login()
 
 
 #########################################物件區######################################################
 url_login = 'http://stucis.ttu.edu.tw/login.php'
 url_listed = 'http://stucis.ttu.edu.tw/selcourse/ListSelected.php'
 url_select = 'http://stucis.ttu.edu.tw/selcourse/DoAddDelSbj.php?'
-header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1897.3 Safari/537.36"}
+header = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1897.3 Safari/537.36'}
 cookie = http.cookiejar.CookieJar()
 flag = True
 ########################################主程式區######################################################
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie)) #cookie處理
 urllib.request.install_opener(opener)
 
-clrscr()
 Login()
 while True:
 	clrscr()
 	showMenu()
 	select = input('select = ')
 	if select == '1':
-		readSelectedList()
-	elif select == '2':
 		selectCourse()
+	elif select == '2':
+		readSelectedList()
 	elif select == '5':
 		print('\n\nGood Bye!!!!')
 		os._exit(0)
-	input('Enter鍵繼續......') 
-
-
-
-
-
-
