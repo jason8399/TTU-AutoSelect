b510a6df54db8fbad6edf429166fdd3e32a2d743
JasonPan <jason8399@gmail.com>
2015-08-10 17:10:13 +0800

    Remove sleep, modify idot method

--- AutoSelect.py
+++ AutoSelect.py
@@ -53,20 +53,18 @@ class AutoSelect:
         self.response = requests.get(self.urlSeltop, headers=self.headers, cookies=self.cookies)
         self.response = requests.get(self.urlListed, headers=self.headers, cookies=self.cookies)
         for y in range(0, 10):
-            for x in range(0, len(self.__courseList)):
-                params = {"AddSbjNo": self.__courseList[x]}
+            for courseId in self.__courseList:
+                params = {"AddSbjNo": courseId}
                 try:
                     self.response = requests.get(self.urlSelect, params=params, headers=self.headers,
                                                  cookies=self.cookies)
                 except TimeoutError:
                     pass
                 print(self.response.status_code)
-                time.sleep(1)
 
     def check_time(self):
         #unavailable
         pass
-
 
 if __name__ == "__main__":
     auto = AutoSelect()
